a4392e093ac139219af82d5018d1aefaad8b76a8
Vishesh19946 <visheshbhandare41@gmail.com>
2022-08-27 01:54:51 +0530

    testing api

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from flask import Flask
+from flask_restful import Resource, Api
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+api = Api(app)
 
 
-# Press the green button in the gutter to run the script.
+class HelloWorld(Resource):
+    def __get__(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, 'https://seek-community.netlify.app/')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
