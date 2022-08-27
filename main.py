715f2b8fb75349de44a3cbfe313abc4ec82fab4b
Vishesh19946 <visheshbhandare41@gmail.com>
2022-08-27 21:20:23 +0530

    First MIgrations

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
 
