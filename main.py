bc3e80b19b0c5478b275877fe97475328396a732
Aniket Ghorpade <aniketgh@amdocs.com>
2022-08-27 21:06:41 +0530

    Models created

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+
 api = Api(app)
 CORS(app)
 
 
-class status (Resource):
+class status(Resource):
     def get(self):
         try:
             return {'data': 'Api is Running'}
@@ -17,7 +27,7 @@ class status (Resource):
 
 class Sum(Resource):
     def get(self, a, b):
-        return jsonify({'data': a+b})
+        return jsonify({'data': a + b})
 
 
 api.add_resource(status, '/')
---
+++ models.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import generate_password_hash, check_password_hash
+from main import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(64), index=True)
+    image_url = db.Column(db.String(64), index=True)
+
+    def __int__(self, username, email, password, img_url):
+        self.username = username
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+        self.img_url = img_url
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"Username: {self.username}"
+
+
+class Professional(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(64), index=True)
+    calendly_url = db.Column(db.String(64), index=True)
+    skill_level = db.Column(db.String(64), index=True)
+    category = db.Column(db.String(64), index=True)
+    bio = db.Column(db.String(64), index=True)
+    specialization = db.Column(db.String(64), index=True)
+
+    def __init__(self, username, email, password, calendly_url, skill_level, bio, category, specialization):
+        self.username = username
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+        self.calendly_url = calendly_url
+        self.skill_level = skill_level
+        self.bio = bio
+        self.category = category
+        self.specialization = specialization
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"Username: {self.username}"
